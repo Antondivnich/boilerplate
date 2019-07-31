c5e2905b39264e2b4976726b708284f8b70580ff
Anton Divnič <antondivnich@gmail.com>
2019-07-31 14:06:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log('You are at '+window.location);
+var money = prompt("сколько денег?"); 
+var apples = prompt("сколько яблок?"); 
+var hleb = prompt("сколько хлеба?"); 
+var apple = prompt("сколько за одно яблоко?")*apples; 
+var onehleb = prompt("сколько за батон?")*hleb; 
+var spent = apple+onehleb; 
+var total = money - spent; 
+Boolean(money >= spent);
+
